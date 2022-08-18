92610d8b4c638c703e9a84b5b3c5d0fd7133be68
Knight3050 <972334508@qq.com>
2022-08-18 11:27:34 +0800

    增加Tag组件

---
+++ Assets/0.14.Components/Tag/Scripts/StatusTag.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StatusTag : MonoBehaviour
+{
+    /// <summary>
+    /// 设置tag状态
+    /// </summary>
+    /// <param name="color">16进制</param>
+    /// <param name="content"></param>
+    public void SetStatus(string color, string content)
+    {
+        Color statusColor = new Color();
+        ColorUtility.TryParseHtmlString(color, out statusColor);
+        this.GetComponent<Image>().color = statusColor;
+        this.GetComponentInChildren<Text>().color = statusColor;
+        this.GetComponentInChildren<Text>().text = content;
+    }
+
+    /// <summary>
+    /// 设置tag状态
+    /// </summary>
+    /// <param name="color">颜色</param>
+    /// <param name="content"></param>
+    public void SetStatus(Color color, string content)
+    {
+        this.GetComponent<Image>().color = color;
+        this.GetComponentInChildren<Text>().color = color;
+        this.GetComponentInChildren<Text>().text = content;
+    }
+}
